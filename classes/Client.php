6f145a31a6e15fa5b44f021b76f52d5507381e16
niyonge2000 <niyongerenadiacadeau@gmail.com>
2023-12-06 12:48:19 +0100

    add user + edit+remove+disable +connexion+login

---
+++ classes/Client.php
@@ -0,0 +1,142 @@
+<?php
+include 'lib/Database.php';
+include_once 'lib/Session.php';
+
+
+class Client{
+
+  private $db;
+
+  public function __construct(){
+    $this->db = new Database();
+  }
+
+  // Date formate Method
+   public function formatDate($date){
+      $strtime = strtotime($date);
+    return date('Y-m-d H:i:s', $strtime);
+   }
+
+
+  public function checkExistEmail($email){
+    $sql = "SELECT email from  tbl_client WHERE email = :email";
+    $stmt = $this->db->pdo->prepare($sql);
+    $stmt->bindValue(':email', $email);
+     $stmt->execute();
+    if ($stmt->rowCount()> 0) {
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  public function userRegistration($data){
+    $name = $data['name'];
+    $username = $data['username'];
+    $email = $data['email'];
+    $text = $data['text'];
+    $adress = $data['adress'];
+    $password = $data['password'];
+
+    $checkEmail = $this->checkExistEmail($email);
+
+    if ($name == "" || $username == "" || $email == "" || $adress == "" || $password == "") {
+      $msg = '<div class="alert alert-danger alert-dismissible mt-3" id="flash-msg">
+<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+<strong>Error !</strong> Please, User Registration field must not be Empty !</div>';
+        return $msg;
+    }elseif (strlen($username) < 3) {
+      $msg = '<div class="alert alert-danger alert-dismissible mt-3" id="flash-msg">
+<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+<strong>Error !</strong> Username is too short, at least 3 Characters !</div>';
+        return $msg;
+    }elseif (filter_var($adress,FILTER_SANITIZE_NUMBER_INT) == FALSE) {
+      $msg = '<div class="alert alert-danger alert-dismissible mt-3" id="flash-msg">
+<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+<strong>Error !</strong> Enter only Number Characters for Mobile number field !</div>';
+        return $msg;
+
+    }elseif(strlen($password) < 5) {
+      $msg = '<div class="alert alert-danger alert-dismissible mt-3" id="flash-msg">
+<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+<strong>Error !</strong> Password at least 6 Characters !</div>';
+        return $msg;
+    }elseif(!preg_match("#[0-9]+#",$password)) {
+      $msg = '<div class="alert alert-danger alert-dismissible mt-3" id="flash-msg">
+<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+<strong>Error !</strong> Your Password Must Contain At Least 1 Number !</div>';
+        return $msg;
+    }elseif(!preg_match("#[a-z]+#",$password)) {
+      $msg = '<div class="alert alert-danger alert-dismissible mt-3" id="flash-msg">
+<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+<strong>Error !</strong> Your Password Must Contain At Least 1 Number !</div>';
+        return $msg;
+    }elseif (filter_var($email, FILTER_VALIDATE_EMAIL === FALSE)) {
+      $msg = '<div class="alert alert-danger alert-dismissible mt-3" id="flash-msg">
+<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+<strong>Error !</strong> Invalid email address !</div>';
+        return $msg;
+    }elseif ($checkEmail == TRUE) {
+      $msg = '<div class="alert alert-danger alert-dismissible mt-3" id="flash-msg">
+<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+<strong>Error !</strong> Email already Exists, please try another Email... !</div>';
+        return $msg;
+    }else{
+
+      $sql = "INSERT INTO tbl_users(name, username, email, password, adress, text) VALUES(:name, :username, :email, :password, :adress, :text)";
+      $stmt = $this->db->pdo->prepare($sql);
+      $stmt->bindValue(':name', $name);
+      $stmt->bindValue(':username', $username);
+      $stmt->bindValue(':email', $email);
+      $stmt->bindValue(':password', SHA1($password));
+      $stmt->bindValue(':adress', $adress);
+      $stmt->bindValue(':text', $text);
+      $result = $stmt->execute();
+      if ($result) {
+        $msg = '<div class="alert alert-success alert-dismissible mt-3" id="flash-msg">
+  <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+  <strong>Success !</strong> Wow, you have Registered Successfully !</div>';
+          return $msg;
+      }else{
+        $msg = '<div class="alert alert-danger alert-dismissible mt-3" id="flash-msg">
+  <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+  <strong>Error !</strong> Something went Wrong !</div>';
+          return $msg;
+      }
+
+
+
+    }
+
+
+
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ db_admin2.sql
@@ -0,0 +1,43 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+
+DROP TABLE IF EXISTS `tbl_roles`;
+CREATE TABLE IF NOT EXISTS `tbl_roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'role_id',
+  `role` varchar(255) DEFAULT NULL COMMENT 'role_text',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+
+INSERT INTO `tbl_roles` (`id`, `role`) VALUES
+(1, 'Admin'),
+(2, 'Editor'),
+(3, 'User');
+
+DROP TABLE IF EXISTS `tbl_users`;
+CREATE TABLE IF NOT EXISTS `tbl_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `mobile` varchar(25) DEFAULT NULL,
+  `roleid` tinyint(4) DEFAULT NULL,
+  `isActive` tinyint(4) DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4;
+
+
+INSERT INTO `tbl_users` (`id`, `name`, `username`, `email`, `password`, `mobile`, `roleid`, `isActive`, `created_at`, `updated_at`) VALUES
+(23, 'achref', 'achref', 'achref.nefzazoui@gmail.com', '3ea543d29ad3c1c09fcfbdda3f2f0617c50ab138', '54852852', 1, 0, '2020-12-19 14:35:56', '2020-12-19 14:35:56'),
+(24, 'ahmed', 'benahmed', 'achme@gmail.com', '7f0c9d56d40c3cc1e23e0113d5377779a4de86ff', '54277528', 3, 0, '2020-12-19 15:13:39', '2020-12-19 15:13:39'),
+(25, 'Fathi', 'fathiA', 'fathianh@gmail.com', '0a859b9a4ebbde4f63383bca7e34890985782348', '54672828', 3, 0, '2020-12-19 15:15:52', '2020-12-19 15:15:52'),
+(26, 'Makrem', 'makrem', 'makrem@gmail.com', 'adef7009a84a71c226ddf68671e929d68a707762', '42551771', 3, 0, '2020-12-19 15:16:59', '2020-12-19 15:16:59'),
+(27, 'Sirin', 'Sirin', 'Sirin@gmail.com', '03ee5fda2eae80be34c0142fe28ac6401e63324c', '23451671', 3, 0, '2020-12-19 15:17:34', '2020-12-19 15:17:34');
+COMMIT;
